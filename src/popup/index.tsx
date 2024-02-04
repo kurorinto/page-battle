c2e08bf7930cdbc27b0e9abad8fcf34af94e855a
kororinto <394460456@qq.com>
2024-02-04 17:51:03 +0800

    feat: 添加游戏速度配置项

--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -6,6 +6,8 @@ import { Button } from "./components/ui/button"
 
 import "./index.scss"
 
+import { Slider } from "./components/ui/slider"
+
 const storage = new Storage()
 
 export interface Message {
@@ -23,6 +25,9 @@ const getCurrentTabId = async (): Promise<chrome.tabs.Tab | undefined> => {
 const Popup = () => {
   const [loaded, setLoaded] = useState(false)
   const [started, setStarted] = useState(false)
+  const [rocketSpeed, setRocketSpeed] = useState(5)
+  const [bulletSpeed, setBulletSpeed] = useState(15);
+  const [firingRate, setFiringRate] = useState(10);
 
   const init = async () => {
     const storageStarted = JSON.parse(
@@ -41,13 +46,54 @@ const Popup = () => {
   }, [started])
 
   return (
-    <div className="w-[360px] p-[8px] flex flex-col gap-y-[8px] bg-[#fff]">
+    <div className="w-[360px] p-[8px] flex flex-col gap-y-[12px] bg-[#fff]">
       <div className="text-[20px]">
         <div>Page Battle</div>
         <div className="text-[14px]">在页面上开始你的飞机大战吧！</div>
+        <div className="text-gray-500 text-[12px]">
+          操作说明：W(或↑)前进，A(或←)左转，D(或→)右转。空格发射
+        </div>
       </div>
-      <div className="text-gray-500">
-        操作说明：W(或↑)前进，A(或←)左转，D(或→)右转。空格发射
+      <div className="flex flex-col gap-y-[4px]">
+        <div className="flex items-center">
+          <div className="break-all text-nowrap">飞机速度：</div>
+          <div className="w-[30px] mr-[4px]">({rocketSpeed})</div>
+          <Slider
+            className="flex-1"
+            value={[rocketSpeed]}
+            max={100}
+            step={1}
+            onValueChange={([val]) => {
+              setRocketSpeed(val)
+            }}
+          />
+        </div>
+        <div className="flex items-center">
+          <div className="break-all text-nowrap">子弹速度：</div>
+          <div className="w-[30px] mr-[4px]">({bulletSpeed})</div>
+          <Slider
+            className="flex-1"
+            value={[bulletSpeed]}
+            max={100}
+            step={1}
+            onValueChange={([val]) => {
+              setBulletSpeed(val)
+            }}
+          />
+        </div>
+        <div className="flex items-center">
+          <div className="break-all text-nowrap">子弹射速：</div>
+          <div className="w-[30px] mr-[4px]">({firingRate})</div>
+          <Slider
+            className="flex-1"
+            value={[firingRate]}
+            max={100}
+            step={1}
+            onValueChange={([val]) => {
+              setFiringRate(val)
+            }}
+          />
+        </div>
       </div>
       {loaded && (
         <Button
